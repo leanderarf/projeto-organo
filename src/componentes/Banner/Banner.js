b1ebf4cfe3f3e84989c3c60e021a7fec8fcc12aa
leanderarf <arfleander10@gmail.com>
2025-10-12 15:01:06 -0300

    criação do banner e limpando arquivos não utilizados

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner"> 
+            <img src="./imagens/banner.png" alt="O banner principal da página do Organo" className='banner img'/>
+        </header>
+    )
+}
+
+export default Banner
