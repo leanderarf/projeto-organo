9cfe6dd9a42073564ea645f7d693945c268085fe
leanderarf <arfleander10@gmail.com>
2025-10-14 23:07:20 -0300

    criação e implementação da lista suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,21 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 
 const Formulario = () => {
+
+    const times = [
+        'Programação', 
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    
     return (
         <section className='formulario'>
             <form>
@@ -10,6 +23,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens = {times}/>
             </form>
         </section>
     )
