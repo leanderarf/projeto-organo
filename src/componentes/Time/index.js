04df46b5ce3940258b12d08b847e1a68e9e62f49
leanderarf <arfleander10@gmail.com>
2025-10-18 18:46:10 -0300

    escondendo os times e limpando o formulário

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,12 +5,13 @@ const Time = (props) => {
     const css = {backgroundColor: props.corPrimaria}
 
     return (    
-        <section className='time' style={css}>
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
             <h3 style={{ borderColor: props.corSecundaria }}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
             </div>
         </section>
+        : ''
     )
 }
 
